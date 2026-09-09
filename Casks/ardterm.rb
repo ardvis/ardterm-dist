666bbd8033fe11e583e528e4bbd11261d4aa7854
ardhi <ardhi@kertas.co.id>
2026-09-09 14:16:56 +0700

    Publish Ardterm v0.1.4 cask

--- Casks/ardterm.rb
+++ Casks/ardterm.rb
@@ -1,6 +1,6 @@
 cask "ardterm" do
-  version "0.1.3"
-  sha256 "2a534f436b19d115989beca8d9aa21ca4dae8530efd92f55440c479711036db5"
+  version "0.1.4"
+  sha256 "fefbf9468b7702a08e12251e7a8eecfc977ad991d70356a26d1b8a6d1be02cf6"
   url "https://github.com/ardvis/ardterm-dist/releases/download/v#{version}/Ardterm-macos-arm64.zip"
   name "Ardterm"
   desc "Native macOS terminal with authenticated remote sessions"
